2337a3503fa1a09e5ce9df3764d1c3ea5d42c89c
Jasper Ginn <jasperginn@gmail.com>
2018-08-10 14:53:36 +0200

    Update historic.py

--- FinTxtClient/routes/historic.py
+++ FinTxtClient/routes/historic.py
@@ -51,7 +51,7 @@ class historic(object):
         free = current - datetime.timedelta(30)
 
         # Needs key if want this endpoint
-        if date_conv >= free:
+        if date_conv.date() >= free:
 
             self._apiClient._requires_key = True
 
